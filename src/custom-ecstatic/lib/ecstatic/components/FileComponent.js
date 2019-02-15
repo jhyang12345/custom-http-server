f6af4f78ec7891ea3eadf08661ee2c5bfd0042e3
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-02-15 23:54:05 +0900

    Began applying styled components

---
+++ src/custom-ecstatic/lib/ecstatic/components/FileComponent.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class FileComponent extends React.Component {
+    render() {
+        return (
+            <div>File</div>
+        )
+    }
+}
+
+export default FileComponent
