979431a23ffad3721d8603415618ec8ab8b9ce1f
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-02-19 14:50:10 +0900

    Reconfigured webpack to work, but babel stopped working...

--- src/custom-ecstatic/lib/ecstatic/client/index.js
+++ src/custom-ecstatic/lib/ecstatic/client/index.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import ReactDom from 'react-dom'
+import App from '../components/App'
 
 console.log("Loaded?")
+
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
- }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", 
+        query: {
+          presets: ['@babel/react', '@babel/env']
+        } 
+      }
+    ],
+ },
+  node: {
+    fs: 'empty'
+  }
 }
