770f44a21f5c1e0f31b9319bcf38468b65dcff55
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-02-27 14:00:23 +0900

    Changed styling to show border-bottom noit completely sure why it works yet

--- src/custom-ecstatic/lib/ecstatic/components/Search/SearchBarComponent.js
+++ src/custom-ecstatic/lib/ecstatic/components/Search/SearchBarComponent.js
@@ -34,14 +34,20 @@ const SearchBar = styled.input.attrs({
     display: inline-block;
     width: 200px;
     height: 30px;
-    margin-right: 8px;
+    margin-right: 4px;
+    padding-left: 4px;
+    padding-right: 4px;
     outline: none;
-    border: none;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    vertical-align: middle;
     border-bottom: 1px solid #AAA;
-    z-index: 0;
     transform: ${props => props.revealed === true ? 'translateX(0%)' : 'translateX(100%)'};
     opacity: ${props => props.revealed === true ? '1' : '0'};
     transition: transform 0.3s ease-out, opacity 0.3s ease-out;
+    font-size: 0.7em;
+    color: #333;
 `
 
 export default SearchBarComponent
--- src/custom-ecstatic/lib/ecstatic/components/SearchComponent.js
+++ src/custom-ecstatic/lib/ecstatic/components/SearchComponent.js
@@ -34,19 +34,13 @@ class SearchComponent extends React.Component {
 const SearchBox = styled.span`
     position: absolute;
     height: 100%;
-    line-height: 60px;
+    line-height: 58px;
     right: 0px;
     top: 0px;
     padding-right: 12px;
     display: inline-box;
     margin-left: auto;
     overflow: hidden;  
-
-    & > .fas {
-        font-size: 0.6em;
-        color: #999;
-        vertical-align: middle;
-    }
 `
 
 export default SearchComponent
