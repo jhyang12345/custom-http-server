bc03a003bf0559b42be667c9b4f11269583b31a0
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-03-01 22:20:53 +0900

    Adding keyframe animation

--- src/custom-ecstatic/lib/ecstatic/components/Search/SearchButtonComponent.js
+++ src/custom-ecstatic/lib/ecstatic/components/Search/SearchButtonComponent.js
@@ -1,25 +1,52 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 
 class SearchButtonComponent extends React.Component {
 
+    state = {
+        animate: false,
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.revealed != this.props.revealed) {
+            this.setState(() => ({
+                animate: true,
+            }))
+        }
+    }
+
     render() {
         const { handleFocus, revealed } = this.props
+        console.log("animate", this.state.animate)
         return (
             <SearchButton
                 onClick={handleFocus}
-                style={{ zIndex: 1 }}>
+                style={{ zIndex: 1 }}
+                animate={this.state.animate}
+                >
                 {(
                     revealed === true 
                     ? <i className="fas fa-times"></i>
                     : <i className="fas fa-search"></i>
                 )}
-                
             </SearchButton>
-  
         )
     }
 }
+
+const iconAnimation = keyframes`
+    0% {
+        opacity: 1;
+    }
+
+    50% {
+        opacity: 0;
+    }
+
+    100% {
+        opacity: 1;
+    }
+`
 
 const SearchButton = styled.span`
     display: inline-block;
@@ -28,6 +55,8 @@ const SearchButton = styled.span`
     padding: 8px;
     position: relative;
 
+    animation: ${iconAnimation} 1s ease-in-out;
+    
     & > .fas {
         font-size: 0.6em;
         color: #999;
