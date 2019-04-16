726a0ce8b6480b58a78eec822de2dda0afa3b80b
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-04-16 20:44:14 +0900

    Dealt with hidden popup issue

--- src/custom-ecstatic/lib/ecstatic/components/Popup/PopupComponent.js
+++ src/custom-ecstatic/lib/ecstatic/components/Popup/PopupComponent.js
@@ -19,8 +19,27 @@ class PopupComponent extends React.Component {
     }
 
     componentDidUpdate() {
-        if(this.props.visible === true && this.props.hidden == false) {
-            
+        if(this.props.visible === true &&
+             this.props.hidden == false             
+            ) {
+            const { clientX, clientY } = this.props
+            const popupElem = this.popupRef.current
+            const width = popupElem.offsetWidth
+            const height = popupElem.offsetHeight
+            let newLeft = 0
+            let newTop = 0
+            if (clientX + width > window.innerWidth) {
+                newLeft = -width
+            }
+            if (clientY + height > window.innerHeight) {
+                newTop = -height
+            }
+            if(newLeft == this.state.left &&
+                newTop == this.state.top) return
+            this.setState(() => ({
+                left: newLeft,
+                top: newTop,
+            }))
         }
     }
 
@@ -31,7 +50,10 @@ class PopupComponent extends React.Component {
             open,
             visible,
             hidden
-          } = this.props;
+          } = this.props
+        const {
+            top, left
+        } = this.state
         return (
             <Popup
                 className="block-copy"
@@ -39,8 +61,8 @@ class PopupComponent extends React.Component {
                 onContextMenu={this.props.closePopup}
                 visible={visible}
                 hidden={hidden}
-                clientX={clientX}
-                clientY={clientY}
+                clientX={clientX + left}
+                clientY={clientY + top}
                 ref={this.popupRef}
             >
                 <PopupItem onClick={this.props.copyLink}>Copy Link</PopupItem>
