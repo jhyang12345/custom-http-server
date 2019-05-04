f1a3949d8459f791a0371f131ebed53d66e9541d
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-05-04 17:18:28 +0900

    Moved persist config out

--- src/custom-ecstatic/lib/ecstatic/reducers/index.js
+++ src/custom-ecstatic/lib/ecstatic/reducers/index.js
@@ -3,9 +3,17 @@ import currentDirectory from './currentDirectory'
 import optionPopup from './optionPopup'
 import viewState from './viewState'
 import { loadingBarReducer } from "react-redux-loading"
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const curDirConfig = {
+    key: 'currentDirectory',
+    storage: storage,
+    blacklist: ["pathName", "keyword", "contents", "visibleContent"]
+}
 
 export default combineReducers({
-    currentDirectory,
+    currentDirectory: persistReducer(curDirConfig, currentDirectory),
     optionPopup,
     viewState,
     loadingBar: loadingBarReducer,
